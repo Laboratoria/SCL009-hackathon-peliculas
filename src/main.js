91045478dc00606a64428749282dd73141fd1825
GelenMR <gelen.montilla.r@gmail.com>
2019-05-21 20:03:51 -0400

    Flitro por genero en proceso (en consola)

--- src/main.js
+++ src/main.js
@@ -13,23 +13,22 @@ btn.addEventListener('click', () => {
     searchFilms()
 })
 const searchFilms = () => {
-    // GUARDA EL NOMBRE DE LA PELICULA PARA CONCATENARLO CON LA URL
-    //const nameMovie = '&s='+ document.getElementById('nameMovie').value
-   // const nameMovie = 't='+document.getElementById('nameMovie').value+'&apikey=857ae5f9'
-    const nameMovie = 's='+document.getElementById('nameMovie').value+'&apikey=857ae5f9'
-    fetch(url+nameMovie)
+// GUARDA EL NOMBRE DE LA PELICULA PARA CONCATENARLO CON LA URL
+// const nameMovie = '&s='+ document.getElementById('nameMovie').value
+// const nameMovie = 't='+document.getElementById('nameMovie').value+'&apikey=857ae5f9'
+   const nameMovie = 's='+document.getElementById('nameMovie').value+'&apikey=857ae5f9'
+   fetch(url+nameMovie)
     .then(response => response.json())
     .then(response => { response.Search
-       
        console.log(response.Search);
        /* var title = response.Title;
         var image = response.Poster;
         genre = response.Genre;
         plot = response.Plot;*/
 
-        let element = document.getElementById("element")
+        let myContent = document.getElementById("myContent")
     
-        //por titulo
+    //por titulo
        /* element.innerHTML = 
         `  
         <p>${response.Title}</p>
@@ -41,59 +40,84 @@ const searchFilms = () => {
         `
         */
         
-        //por busqueda
-        
+    //por busqueda
         for(let index = 0; index < response.Search.length; index++){
         if(response.Search[index].Type=="movie"){
-            element.innerHTML+=
-    `
-       
-    
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3">    
-    <div class="card bg-light mb-3" align= "middle">
-    <img src= "${response.Search[index].Poster}" class= "card-img-top" alt= "Card image" id="cardImage">
-    <div class="card-body text-dark">
-    <p class="card-name">Title: ${response.Search[index].Title}</p>
-    <p class="card-num"> Year: ${response.Search[index].Year}</p>
-    </div>
-    </div>
-    
-  
-        `  
+            myContent.innerHTML+=
+            `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">    
+            <div class="card bg-light mb-3" align= "middle">
+            <img src= "${response.Search[index].Poster}" class= "card-img-top" alt= "Card image" id="cardImage">
+            <div class="card-body text-dark">
+            <p class="card-name">Title: ${response.Search[index].Title}</p>
+            <p class="card-num"> Year: ${response.Search[index].Year}</p>
+            </div>
+            </div>
+            `  
         }   
-    }
+        }
     })
-
     //  <p>${response.Search[index].Title}</p>
-      //  <img src= "${response.Search[index].Poster}" alt="image" >
-       // <p>${response.Search[index].Year}</p>
- 
-
-
-
-      // console.log(url + nameMovie);
-      //console.log(response);  
-      //console.log(response.Search[0].imdbID); 
-        //loadFilms(data)
-    
-    //.then(function(data) {
-       // drawFilms(data)
-       // console.log(data.Title);
-    //})
-
-// const loadFilms = (data) => {
-//     const idFilms = '&i=tt0382268' //debe ser un array con los id de las peliculas a mostrar
-//     let dataMovie = data.results;
-//     document.getElementById('myContent').innerHTML = '';
-// AQUI SE DEBERIA VOLVER A RECORRER LA DATA
-// }
-//
-// const drawFilms = data => {
-//     document.getElementById('myContent').innerHTML +=  
-//     `
-//     <div id="${data.imdbID}" data-target="modal${data.imdbID}" class="modal-trigger "> //PARA LLAMAR E IMPRIMIR EL MODAL        
-//         <img src="${data.Poster}" alt="${data.Title}" />
-//         <p>${data.Title}</p>   
-//     </div>`
-// }
+    //  <img src= "${response.Search[index].Poster}" alt="image" >
+    // <p>${response.Search[index].Year}</p>
 }
+
+// // DECLARACIÓN DE VARIABLES GLOBALES
+// // URL OMDB API + APIKEY 
+// const urlOMdb = 'http://www.omdbapi.com/?apikey=857ae5f9&page=1&plot=full&type=movie';
+// // URL TMDB API + APIKEY
+// const urlTMdb ="https://api.themoviedb.org/3/discover/movie?api_key=665c6e4dc27510e065d9033504dfed36";
+// // URL DE IMG DE POSTER OMDB API + APIKEY
+// const urlPoster = 'http://img.omdbapi.com/?apikey=857ae5f9';
+// const myContent = document.getElementById('myContent');
+// let films = '';
+// let dataFilms = '';
+
+// // BUSCAR PELICULA POR NOMBRE O PALABRA CLAVE
+// btn.addEventListener('click', () => {
+//     searchFilms()
+// })
+// const searchFilms = () => {
+//     // GUARDA EL NOMBRE DE LA PELICULA PARA CONCATENARLO CON LA URL
+//     let nameMovie = '&s='+ document.getElementById('nameMovie').value
+//     fetch(urlOMdb + nameMovie)
+//     .then(response => response.json())
+//     .then((data) => {
+//         console.log(data);
+//         console.log(urlOMdb + nameMovie);
+//         console.log(data.Search.length);
+//         console.log(data.Search[0].Poster); 
+//         drawFilms(data);
+//     });
+// }
+// // FUNCIÓN PARA DIBUJAR DATA EN PANATALLA
+// const drawFilms = (data) => {
+//     myContent.innerHTML = '';
+//     data.results.forEach(element => {
+//     //EL ID DEL DIV ES PARA LLAMAR E IMPRIMIR EL MODAL
+//     films = `
+//         <div id="${element.imdbID}" data-target="modal${element.imdbID}" class="modal-trigger ">         
+//             <img src="${element.Poster}" alt="${element.Title}" />
+//             <p>${element.Title}</p>   
+//         </div>`
+//     dataFilms += films;
+//     });
+//     myContent.innerHTML = dataFilms;
+// }
+
+// // FILTRAR PELICULAS POR GENERO
+// btnAction.addEventListener('click', () => {
+//     genresFilter()
+// })
+// const genresFilter = () => {
+//     let genresFilms = '&with_genres=28'
+//     fetch(urlTMdb+genresFilms)
+//     .then(response => response.json())
+//     .then((data) => {
+//         console.log(data);
+//         console.log(urlTMdb+genresFilms);
+//         console.log(data.results.length);
+//         console.log(data.results[0].title); 
+//         drawFilms(data);
+//     });
+// }
